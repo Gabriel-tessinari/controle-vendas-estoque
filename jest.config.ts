c79384a3823b849de416909b7a7908dae33f4071
Gabriel Tessinari <gtcmiranda18@gmail.com>
2025-10-22 21:26:21 -0300

    Configurando jest cobertura mínima 80%

--- jest.config.ts
+++ jest.config.ts
@@ -14,4 +14,12 @@ export default {
     '!src/**/*.spec.ts',
     '!src/**/*.test.ts',
   ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
