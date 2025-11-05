b8079ed07b7e9350052aee1a57350f47b66fa417
Gabriel Tessinari <gtcmiranda18@gmail.com>
2025-11-05 15:30:42 -0300

    Refatorando produtos

--- src/produtos/controllers/produtosController.ts
+++ src/produtos/controllers/produtosController.ts
@@ -1,26 +1,30 @@
 import { Request, Response } from "express";
-import { insertProdutosComVariacoesLote } from "../repositories/produtosRepository";
-import { ProdutoInput } from "../models/ProdutoInput";
 import { BusinessError } from "../../shared/errors/BusinessError";
-import { Produto } from "../models/Produto";
 import { reqEmProdutoInputLote } from "../mappers/reqEmProdutoInputLote";
+import { Produto } from "../models/Produto";
+import { ProdutoInput } from "../models/ProdutoInput";
+import { insertProdutosComVariacoesLote } from "../repositories/produtosRepository";
 
-export async function postProdutosComVariacoesLote(req: Request, res: Response): Promise<void> {
+export async function postProdutosComVariacoesLote(
+  req: Request,
+  res: Response
+): Promise<void> {
   try {
     const produtos: ProdutoInput[] = reqEmProdutoInputLote(req.body);
-    const produtosResponse: Produto[] = await insertProdutosComVariacoesLote(produtos);
+    const produtosResponse: Produto[] = await insertProdutosComVariacoesLote(
+      produtos
+    );
 
     res.status(201).json({
-      message: 'Produtos cadastrados com sucesso.',
-      produtosResponse
+      message: "Produtos cadastrados com sucesso.",
+      produtosResponse,
     });
   } catch (err) {
     if (err instanceof BusinessError) {
       res.status(400).json({ error: err.message });
     } else {
-      console.error('Erro ao cadastrar produtos:', err);
-      res.status(500).json({ error: 'Erro ao cadastrar produtos.' });
+      console.error("Erro ao cadastrar produtos:", err);
+      res.status(500).json({ error: "Erro ao cadastrar produtos." });
     }
   }
 }
-
